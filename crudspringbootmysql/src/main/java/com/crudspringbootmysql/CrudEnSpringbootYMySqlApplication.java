62badde96f9e62bfaddc0d9a8554bda60f538dcd
Diego <diegortizv31@gmail.com>
2020-10-07 18:35:24 -0500

    Proyecto Springboot creado

---
+++ crudspringbootmysql/src/main/java/com/crudspringbootmysql/CrudEnSpringbootYMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.crudspringbootmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEnSpringbootYMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEnSpringbootYMySqlApplication.class, args);
+	}
+
+}
